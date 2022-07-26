59a818c33afbff4eb65de4974e7baf0a65b374d6
Denis GERMAIN <dt.germain@gmail.com>
2022-07-26 10:02:11 +0200

    chore: first commit

--- .gitpod/prepare-k3s.sh
+++ .gitpod/prepare-k3s.sh
@@ -50,6 +50,14 @@ mkdir -p ~/.kube
 echo "✅ k3s server is ready"
 sudo curl -o /usr/bin/kubectl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 sudo chmod +x /usr/bin/kubectl
+curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
+
+helm repo add haproxytech https://haproxytech.github.io/helm-charts
+helm repo update
+helm install haproxy-ingress haproxytech/kubernetes-ingress -n haproxy-ingress --create-namespace
+
+echo "✅ added haproxy as IngressController"
+
 kubectl get pods --all-namespaces
 
 touch "${k3sreadylock}"
